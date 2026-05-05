44b2dae253c7a27ad738d92aaa639a728ff1de24
Erik Uustalu <erik.uustalu@gmail.com>
2026-05-05 23:36:24 +0300

    Glances tools SSH tool Fixed node red reload tool

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.12-slim
 WORKDIR /app
-RUN pip install fastmcp requests
+RUN pip install fastmcp requests paramiko
 COPY server.py .
 EXPOSE 8121
 CMD ["python", "server.py"]
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from fastmcp import FastMCP
 import requests
 import json
 import os
+import paramiko
 
 mcp = FastMCP("mcp-server")
 
@@ -22,10 +23,13 @@ def nodered_update_flows(flows: str) -> str:
     return f"Status: {r.status_code}"
 
 @nodered.tool()
-def nodered_reload_flows() -> str:
+def nodered_reload_flows():
     """Reload node-red flows from disk. Always run this after update_flows"""
-    r = requests.post(f"{NODE_RED_URL}/flows")
-    return f"Status: {r.status_code}"
+    r = requests.post(
+        f"{NODE_RED_URL}/flows",
+        headers={"Node-RED-Deployment-Type": "reload"}
+    )
+    return r.text
 
 # Ntfy
 
@@ -39,7 +43,58 @@ def ntfy_send_notification(title: str, message: str) -> str:
     r = requests.post(NTFY_URL, data=message, headers={"Title": title})
     return f"Status: {r.status_code}"
 
+# Glances
+
+GLANCES_URL = os.environ.get("GLANCES_URL")
+
+glances = FastMCP("glances")
+
+@glances.tool()
+def glances_get_stats_quick() -> str:
+    """Get a quick overview of beelink's stats"""
+    return requests.get(f"{GLANCES_URL}/api/4/quicklook").text
+
+@glances.tool()
+def glances_get_sensors() -> str:
+    """Get beelink's temp sensors data"""
+    return requests.get(f"{GLANCES_URL}/api/4/sensors").text
+
+@glances.tool()
+def glances_get_processes() -> str:
+    """Get beelink's processes data"""
+    return requests.get(f"{GLANCES_URL}/api/4/processlist").text
+
+@glances.tool()
+def glances_get_containers() -> str:
+    """Get beelink's docker containers data"""
+    return requests.get(f"{GLANCES_URL}/api/4/containers").text
+
+# SSH
+
+SSH_HOST = os.environ.get("SSH_HOST")
+SSH_USER = os.environ.get("SSH_USER")
+SSH_PASSWORD = os.environ.get("SSH_PASSWORD")
+
+ssh = FastMCP("ssh")
+
+@ssh.tool()
+def ssh_run_command(command: str, timeout: int) -> str:
+    """Run a command on beelink and return the output"""
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        client.connect(SSH_HOST, username=SSH_USER, password=SSH_PASSWORD, timeout=timeout)
+        stdin, stdout, stderr = client.exec_command(command, timeout=timeout)
+        return stdout.read().decode() + stderr.read().decode()
+    except paramiko.SSHException as e:
+        return f"SSH error: {str(e)}"
+    finally:
+        client.close()
+
 mcp.mount(nodered)
 mcp.mount(ntfy)
+mcp.mount(glances)
+mcp.mount(ssh)
 
 mcp.run(transport="streamable-http", host="0.0.0.0", port=8121)
